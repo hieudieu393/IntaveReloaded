b0733252860d54a70fb39d8db63964d678cca7b9
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-09 16:24:25 +0700

    fix custom payload compatibility

--- src/main/java/com/comphenix/protocol/wrappers/CustomPacketPayloadWrapper.java
+++ src/main/java/com/comphenix/protocol/wrappers/CustomPacketPayloadWrapper.java
@@ -1,2 +1,8 @@
 package com.comphenix.protocol.wrappers;
-public class CustomPacketPayloadWrapper { private final MinecraftKey id; private final byte[] payload; public CustomPacketPayloadWrapper(MinecraftKey id, byte[] payload){this.id=id;this.payload=payload;} public MinecraftKey getId(){return id;} public byte[] getPayload(){return payload;} public MinecraftKey getKey(){return id;} public byte[] getData(){return payload;} }
+public class CustomPacketPayloadWrapper {
+  private final MinecraftKey id;
+  private final byte[] payload;
+  public CustomPacketPayloadWrapper(MinecraftKey id, byte[] payload){this.id=id;this.payload=payload;}
+  public CustomPacketPayloadWrapper(byte[] payload, MinecraftKey id){this(id,payload);}
+  public MinecraftKey getId(){return id;} public byte[] getPayload(){return payload;} public MinecraftKey getKey(){return id;} public byte[] getData(){return payload;}
+}
