682821532bd58bfcc7937752f4c722818e89669c
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-09 13:45:16 +0700

    fix: complete compatibility key and listener options

--- src/main/java/com/comphenix/protocol/events/ListenerOptions.java
+++ src/main/java/com/comphenix/protocol/events/ListenerOptions.java
@@ -1,2 +1,2 @@
 package com.comphenix.protocol.events;
-public enum ListenerOptions { ASYNC }
+public enum ListenerOptions { ASYNC, SKIP_PLUGIN_VERIFIER }
--- src/main/java/com/comphenix/protocol/wrappers/MinecraftKey.java
+++ src/main/java/com/comphenix/protocol/wrappers/MinecraftKey.java
@@ -1,7 +1,10 @@
 package com.comphenix.protocol.wrappers;
 import com.comphenix.protocol.reflect.EquivalentConverter;
 public final class MinecraftKey {
-  private final String fullKey; public MinecraftKey(String key){this.fullKey=key;} public String getFullKey(){return fullKey;} public String getKey(){int i=fullKey.indexOf(':');return i<0?fullKey:fullKey.substring(i+1);}
+  private final String fullKey;
+  public MinecraftKey(String key){this.fullKey=key;}
+  public MinecraftKey(String prefix, String key){this.fullKey=prefix + ":" + key;}
+  public String getFullKey(){return fullKey;} public String getKey(){int i=fullKey.indexOf(':');return i<0?fullKey:fullKey.substring(i+1);}
   public String getPrefix(){int i=fullKey.indexOf(':');return i<0?"minecraft":fullKey.substring(0,i);}
   public static MinecraftKey fromHandle(Object handle){return handle==null?null:new MinecraftKey(String.valueOf(handle));}
   public static EquivalentConverter<MinecraftKey> getConverter(){return new EquivalentConverter<MinecraftKey>(){public Object getGeneric(MinecraftKey k){return k==null?null:k.fullKey;} public MinecraftKey getSpecific(Object o){return MinecraftKey.fromHandle(o);} public Class<MinecraftKey> getSpecificType(){return MinecraftKey.class;}};}
