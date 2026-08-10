0e3432ee7f93bf7c364f2722124cf4dc2d16f24c
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-10 12:23:36 +0700

    combat: inspect cancelled freeze attacks

--- src/main/java/de/jpx3/intave/check/combat/heuristics/modern/CombatFreezeHeuristic.java
+++ src/main/java/de/jpx3/intave/check/combat/heuristics/modern/CombatFreezeHeuristic.java
@@ -34,7 +34,7 @@ public final class CombatFreezeHeuristic extends ModernCombatHeuristic<CombatFre
     super(parentCheck, Meta.class);
   }
 
-  @PacketSubscription(priority = LOW, packetsIn = {ATTACK_ENTITY, USE_ENTITY})
+  @PacketSubscription(priority = LOW, packetsIn = {ATTACK_ENTITY, USE_ENTITY}, ignoreCancelled = false)
   public void receiveAttackPacket(PacketEvent event) {
     EntityUseReader reader = PacketReaders.readerOf(event.getPacket());
     try {
