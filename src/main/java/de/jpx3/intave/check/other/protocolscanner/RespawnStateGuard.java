b8569b3c3747a9588695b9ef8f5e93561bf34781
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-10 20:07:10 +0700

    Add experimental respawn-alive state guard

---
+++ src/main/java/de/jpx3/intave/check/other/protocolscanner/RespawnStateGuard.java
@@ -0,0 +1,68 @@
+package de.jpx3.intave.check.other.protocolscanner;
+
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientClientStatus;
+import com.comphenix.protocol.events.PacketEvent;
+import de.jpx3.intave.check.MetaCheckPart;
+import de.jpx3.intave.check.other.ProtocolScanner;
+import de.jpx3.intave.module.Modules;
+import de.jpx3.intave.module.linker.packet.PacketSubscription;
+import de.jpx3.intave.module.violation.Violation;
+import de.jpx3.intave.user.User;
+import de.jpx3.intave.user.meta.AbilityMetadata;
+import de.jpx3.intave.user.meta.CheckCustomMetadata;
+import de.jpx3.intave.user.meta.MovementMetadata;
+
+import static de.jpx3.intave.module.linker.packet.PacketId.Client.CLIENT_COMMAND;
+
+/** Experimental: detects repeated PERFORM_RESPAWN requests while the tracked player is alive. */
+public final class RespawnStateGuard extends MetaCheckPart<ProtocolScanner, RespawnStateGuard.Meta> {
+  public RespawnStateGuard(ProtocolScanner parentCheck) {
+    super(parentCheck, Meta.class);
+  }
+
+  @PacketSubscription(packetsIn = CLIENT_COMMAND, ignoreCancelled = false)
+  public void receive(PacketEvent event) {
+    if (!(event.delegate() instanceof PacketReceiveEvent)) {
+      return;
+    }
+    WrapperPlayClientClientStatus wrapper = new WrapperPlayClientClientStatus((PacketReceiveEvent) event.delegate());
+    if (wrapper.getAction() != WrapperPlayClientClientStatus.Action.PERFORM_RESPAWN) {
+      return;
+    }
+
+    User user = userOf(event.getPlayer());
+    AbilityMetadata abilities = user.meta().abilities();
+    MovementMetadata movement = user.meta().movement();
+    Meta meta = metaOf(user);
+
+    boolean deathTransition = movement.inRespawnScreen
+      || abilities.health <= 0.0f
+      || abilities.unsynchronizedHealth <= 0.0f;
+    if (deathTransition || user.justJoined()) {
+      meta.buffer = Math.max(0.0, meta.buffer - 0.75);
+      return;
+    }
+
+    // This remains intentionally non-cancelling. A plugin can create unusual death-menu flows;
+    // repeated impossible requests are useful evidence, but should not block recovery from them.
+    meta.buffer += 1.0;
+    if (meta.buffer < 2.0) {
+      return;
+    }
+
+    Violation violation = Violation.builderFor(ProtocolScanner.class)
+      .forPlayer(user.player())
+      .withCheckName("BadPackets")
+      .withMessage("requested respawn while alive")
+      .withDetails("health=" + abilities.health + ", clientHealth=" + abilities.unsynchronizedHealth)
+      .withVL(2.0)
+      .build();
+    Modules.violationProcessor().processViolation(violation);
+    meta.buffer = 1.0;
+  }
+
+  public static final class Meta extends CheckCustomMetadata {
+    private double buffer;
+  }
+}
