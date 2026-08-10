3eb8a19c28e8ad0cc2ecf530d9a38a5e659e2fe0
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-10 20:48:11 +0700

    Expose Elytra check name

--- src/main/java/de/jpx3/intave/check/CheckNames.java
+++ src/main/java/de/jpx3/intave/check/CheckNames.java
@@ -19,6 +19,7 @@ public final class CheckNames {
     names.put("movementsignalguard", "Simulate");
     names.put("airstuckguard", "NoFall");
     names.put("groundspoofguard", "NoFall");
+    names.put("elytrasignalguard", "Elytra");
     names.put("interactionraytrace", "Interact");
     names.put("heuristics", "KillAura");
     names.put("attackraytrace", "Reach");
