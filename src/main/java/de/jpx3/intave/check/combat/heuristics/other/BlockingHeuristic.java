e2df8eb2e7e5aeef67039aa64a906083652a6cc5
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-11 17:33:04 +0700

    Migrate blocking heuristic off ProtocolLib

--- src/main/java/de/jpx3/intave/check/combat/heuristics/other/BlockingHeuristic.java
+++ src/main/java/de/jpx3/intave/check/combat/heuristics/other/BlockingHeuristic.java
@@ -2,21 +2,12 @@
  * Copyright 2026 Intave
  *
  * This software is licensed under the PolyForm Perimeter License 1.0.0.
- * You may use this software for any purpose, except for providing to
- * others any product that competes with the software.
- *
- * A copy of the license is available at:
- *   https://polyformproject.org/licenses/perimeter/1.0.0/
  */
-
 package de.jpx3.intave.check.combat.heuristics.other;
 
-import com.github.retrooper.packetevents.protocol.player.DiggingAction;
-import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.protocol.packettype.PacketTypeCommon;
-import com.comphenix.protocol.events.PacketContainer;
 import com.github.retrooper.packetevents.event.ProtocolPacketEvent;
-import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.DiggingAction;
 import de.jpx3.intave.check.combat.Heuristics;
 import de.jpx3.intave.check.combat.heuristics.ClassicHeuristic;
 import de.jpx3.intave.check.combat.heuristics.HeuristicsClassicType;
@@ -26,227 +17,139 @@ import de.jpx3.intave.executor.Synchronizer;
 import de.jpx3.intave.module.linker.packet.ListenerPriority;
 import de.jpx3.intave.module.linker.packet.PacketSubscription;
 import de.jpx3.intave.module.mitigate.AttackNerfStrategy;
-import de.jpx3.intave.packet.PacketSender;
+import de.jpx3.intave.packet.reader.BlockDigReader;
+import de.jpx3.intave.packet.reader.BlockInteractionReader;
+import de.jpx3.intave.packet.reader.PacketReaders;
 import de.jpx3.intave.user.User;
 import de.jpx3.intave.user.meta.CheckCustomMetadata;
 import de.jpx3.intave.user.meta.ProtocolMetadata;
 import de.jpx3.intave.user.meta.PunishmentMetadata;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static de.jpx3.intave.check.movement.physics.environment.MoveMetric.TELEPORT;
 import static de.jpx3.intave.module.linker.packet.PacketId.Client.*;
 import static de.jpx3.intave.user.meta.ProtocolMetadata.VER_1_9;
 
 public final class BlockingHeuristic extends ClassicHeuristic<BlockingHeuristic.BlockingMeta> {
-
-	public BlockingHeuristic(Heuristics parentCheck) {
+  public BlockingHeuristic(Heuristics parentCheck) {
     super(parentCheck, HeuristicsClassicType.BLOCKING, BlockingMeta.class);
-	}
+  }
 
-  @PacketSubscription(
-    packetsIn = {
-      ARM_ANIMATION, FLYING, LOOK, POSITION, POSITION_LOOK
-    }
-  )
+  @PacketSubscription(packetsIn = {ARM_ANIMATION, FLYING, LOOK, POSITION, POSITION_LOOK})
   public void receiveMovementAndSwingPacket(ProtocolPacketEvent event) {
     Player player = event.getPlayer();
     User user = userOf(player);
     BlockingMeta meta = metaOf(user);
     SimulationEnvironment movementData = user.meta().movement();
-
-    if (movementData.ticksPast(TELEPORT) == 0) {
-      return;
-    }
-
+    if (movementData.ticksPast(TELEPORT) == 0) return;
     if (event.getPacketType() != PacketType.Play.Client.ANIMATION) {
       meta.releasedItemAfterClientTick = false;
       meta.ticksBetweenBlockAndUnblock++;
     }
-    if (meta.ventosFreundlicherBoolean) {
-      meta.clientTicksBetweenBlockingToggle++;
-    }
+    if (meta.ventosFreundlicherBoolean) meta.clientTicksBetweenBlockingToggle++;
     meta.heldItemOperations = 0;
   }
 
-  private void receiveExcludedPacket(Player player, PacketContainer packet) {
-    userOf(player).ignoreNextInboundPacket();
-    PacketSender.receiveClientPacketFrom(player, packet);
-  }
-
-  @PacketSubscription(
-    packetsIn = {
-      BLOCK_PLACE, BLOCK_DIG
-    }
-  )
+  @PacketSubscription(packetsIn = {BLOCK_PLACE, BLOCK_DIG})
   public void receiveInteractionPacket(ProtocolPacketEvent event) {
     Player player = event.getPlayer();
     User user = userOf(player);
     PunishmentMetadata punishmentData = user.meta().punishment();
     BlockingMeta meta = metaOf(user);
-    PacketContainer packet = event.getPacket();
+    if (!user.meta().protocol().emptyFlyingPacketsAreExplicitlySent()
+      || user.meta().abilities().ignoringMovementPackets()
+      || user.meta().movement().ticksPast(TELEPORT) < 10) return;
 
-    if (!user.meta().protocol().emptyFlyingPacketsAreExplicitlySent() || user.meta().abilities().ignoringMovementPackets() || user.meta().movement().ticksPast(TELEPORT) < 10) {
+    if (event.getPacketType() == PacketType.Play.Client.PLAYER_DIGGING) {
+      BlockDigReader reader = PacketReaders.readerOf(event);
+      try {
+        if (reader.action() == DiggingAction.RELEASE_USE_ITEM) {
+          meta.releasedItemAfterClientTick = true;
+          meta.ventosFreundlicherBoolean = true;
+          int ticksBetweenBlockAndUnblock = meta.ticksBetweenBlockAndUnblock;
+          if (ticksBetweenBlockAndUnblock == 0) {
+            flag(player, "unblocked too quickly (" + ticksBetweenBlockAndUnblock + ")");
+            user.nerf(AttackNerfStrategy.BLOCKING, "block:speed");
+            punishmentData.timeLastBlockCancel = System.currentTimeMillis();
+            Synchronizer.synchronize(() -> DataWatcherAccess.setDataWatcherFlag(player, DataWatcherAccess.WATCHER_BLOCKING_ID, false));
+          }
+        }
+      } finally {
+        reader.release();
+      }
       return;
     }
 
-    if (packet.getType() == PacketType.Play.Client.PLAYER_DIGGING) {
-      DiggingAction playerDigType = packet.getPlayerDigTypes().readSafely(0);
-      if (playerDigType == DiggingAction.RELEASE_USE_ITEM) {
-        meta.releasedItemAfterClientTick = true;
-        meta.ventosFreundlicherBoolean = true;
-
-        int ticksBetweenBlockAndUnblock = meta.ticksBetweenBlockAndUnblock;
-        if (ticksBetweenBlockAndUnblock == 0) {
-          String description = "unblocked too quickly (" + ticksBetweenBlockAndUnblock + ")";
-          flag(player, description);
-          //dmc6
-          user.nerf(AttackNerfStrategy.BLOCKING, "block:speed");
-          punishmentData.timeLastBlockCancel = System.currentTimeMillis();
-          Synchronizer.synchronize(() -> DataWatcherAccess.setDataWatcherFlag(player, DataWatcherAccess.WATCHER_BLOCKING_ID, false));
-        }
-
-      }
-    } else { // BLOCK_PLACE
-      ItemStack itemInHand = packet.getItemModifier().readSafely(0);
-      boolean sword = itemInHand != null && itemInHand.getType().name().endsWith("_SWORD");
-
+    BlockInteractionReader reader = PacketReaders.readerOf(event);
+    try {
+      Material itemType = user.meta().inventory().heldItemType();
+      boolean sword = itemType != null && itemType.name().endsWith("_SWORD");
       if (meta.releasedItemAfterClientTick) {
-        String description = "sent multiple blocking interactions per tick (" + (itemInHand == null ? "null" : itemInHand.getType()) + ")";
-        flag(player, description);
+        flag(player, "sent multiple blocking interactions per tick (" + itemType + ")");
         user.nerf(AttackNerfStrategy.BLOCKING, "block:multiple");
       }
-
       int clientTicksBetweenBlockingToggle = meta.clientTicksBetweenBlockingToggle;
-      Integer integer = packet.getIntegers().readSafely(0);
-      if (integer == null) {
-        integer = 0;
-      }
-      if (integer == 255 && meta.ventosFreundlicherBoolean && sword) {
+      if (reader.enumDirection() == 255 && meta.ventosFreundlicherBoolean && sword) {
         meta.clientTicksBetweenBlockingToggle = 0;
         meta.ventosFreundlicherBoolean = false;
-
         if (clientTicksBetweenBlockingToggle == 0 && meta.acaBlockingVL < 20) {
           meta.acaBlockingVL++;
           if (meta.acaBlockingVL > 2) {
-            String description = "sent too few packets between block-toggle packets (vl: " + meta.acaBlockingVL + ")";
-            flag(player, description);
+            flag(player, "sent too few packets between block-toggle packets (vl: " + meta.acaBlockingVL + ")");
             user.nerf(AttackNerfStrategy.BLOCKING, "block:packets");
           }
         } else if (meta.acaBlockingVL > 1) {
           meta.acaBlockingVL -= 2;
         }
       }
-
       meta.ticksBetweenBlockAndUnblock = 0;
+    } finally {
+      reader.release();
     }
   }
 
-  //---------other-check-------------
-
-  @PacketSubscription(
-    priority = ListenerPriority.HIGH,
-    packetsIn = {
-      FLYING, POSITION, POSITION_LOOK, LOOK, VEHICLE_MOVE
-    }
-  )
+  @PacketSubscription(priority = ListenerPriority.HIGH, packetsIn = {FLYING, POSITION, POSITION_LOOK, LOOK, VEHICLE_MOVE})
   public void receiveMovementPacket(ProtocolPacketEvent event) {
     Player player = event.getPlayer();
     User user = userOf(player);
     BlockingMeta meta = metaOf(user);
     SimulationEnvironment movementData = user.meta().movement();
     ProtocolMetadata clientData = user.meta().protocol();
-    if (movementData.ticksPast(TELEPORT) < 10) {
-      return;
-    }
-    // checks if the client version is above 1.8 for disabling the check if the player is standing still
+    if (movementData.ticksPast(TELEPORT) < 10) return;
     if (!movementData.receivedFlyingPacketIn(2) || clientData.protocolVersion() < VER_1_9) {
-      if (meta.heldItemOperations > 0) {
-        if (meta.blocksPlacedThisTick == 0 || meta.heldItemOperations > 2) {
-          String description = "sent too many item operations (operations: " + meta.heldItemOperations + ")";
-          description += " (version " + user.meta().protocol().versionString() + ")";
-          flag(player, description);
-          meta.unsendPackets.clear();
-        }
+      if (meta.heldItemOperations > 0 && (meta.blocksPlacedThisTick == 0 || meta.heldItemOperations > 2)) {
+        String description = "sent too many item operations (operations: " + meta.heldItemOperations + ")";
+        description += " (version " + user.meta().protocol().versionString() + ")";
+        flag(player, description);
       }
     }
-
-//    if(meta.unsendPackets.size() != 0) {
-//      PacketContainer packetContainer = meta.unsendPackets.get(0);
-//      receiveExcludedPacket(player, packetContainer);
-//      meta.unsendPackets.clear();
-//    }
-
     meta.blocksPlacedThisTick = 0;
   }
 
-  @PacketSubscription(
-    packetsIn = {
-      USE_ITEM
-    }
-  )
+  @PacketSubscription(packetsIn = USE_ITEM)
   public void receiveUseItem(ProtocolPacketEvent event) {
-    Player player = event.getPlayer();
-    User user = userOf(player);
-    ProtocolMetadata clientData = user.meta().protocol();
-    BlockingMeta meta = metaOf(player);
-    // 1.8
-    if (clientData.protocolVersion() >= VER_1_9) {
-      meta.blocksPlacedThisTick++;
-    }
+    User user = userOf(event.getPlayer());
+    if (user.meta().protocol().protocolVersion() >= VER_1_9) metaOf(user).blocksPlacedThisTick++;
   }
 
-  @PacketSubscription(
-    packetsIn = {
-      BLOCK_PLACE
-    }
-  )
+  @PacketSubscription(packetsIn = BLOCK_PLACE)
   public void receiveBlockPlace(ProtocolPacketEvent event) {
-    Player player = event.getPlayer();
-    User user = userOf(player);
-    BlockingMeta meta = metaOf(player);
-    ProtocolMetadata clientData = user.meta().protocol();
-    // 1.9+
-    if (clientData.protocolVersion() < VER_1_9) {
-      meta.blocksPlacedThisTick++;
-    }
+    User user = userOf(event.getPlayer());
+    if (user.meta().protocol().protocolVersion() < VER_1_9) metaOf(user).blocksPlacedThisTick++;
   }
 
-  @PacketSubscription(
-    packetsIn = {
-      HELD_ITEM_SLOT_IN
-    }
-  )
+  @PacketSubscription(packetsIn = HELD_ITEM_SLOT_IN)
   public void receiveHeldItemSlot(ProtocolPacketEvent event) {
-    Player player = event.getPlayer();
-    User user = userOf(player);
-    BlockingMeta meta = metaOf(player);
-    ProtocolMetadata clientData = user.meta().protocol();
-    if (user.meta().abilities().ignoringMovementPackets()) {
-      return;
-    }
-
-//    if(!movementData.recentlyEncounteredFlyingPacket(2) || clientData.protocolVersion() < VER_1_9) {
-//      if (meta.heldItemOperations > 0) {
-//        PacketContainer clonedPacket = event.getPacket().deepClone();
-//        meta.unsendPackets.add(clonedPacket);
-//        event.setCancelled(true);
-//      }
-//    }
-
-    meta.heldItemOperations++;
+    User user = userOf(event.getPlayer());
+    if (!user.meta().abilities().ignoringMovementPackets()) metaOf(user).heldItemOperations++;
   }
 
   public static final class BlockingMeta extends CheckCustomMetadata {
-    private final List<PacketContainer> unsendPackets = new ArrayList<>();
     private int blocksPlacedThisTick;
     public boolean releasedItemAfterClientTick;
     public int ticksBetweenBlockAndUnblock, clientTicksBetweenBlockingToggle;
     public boolean ventosFreundlicherBoolean;
-
     public int acaBlockingVL;
     public int heldItemOperations;
   }
