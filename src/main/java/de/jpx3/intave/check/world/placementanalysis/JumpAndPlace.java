c368675b18780d27c25487ad230687b25d1454af
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-11 17:31:13 +0700

    Migrate jump placement analysis to native PacketEvents reader

--- src/main/java/de/jpx3/intave/check/world/placementanalysis/JumpAndPlace.java
+++ src/main/java/de/jpx3/intave/check/world/placementanalysis/JumpAndPlace.java
@@ -1,6 +1,5 @@
 package de.jpx3.intave.check.world.placementanalysis;
 
-import com.comphenix.protocol.events.PacketContainer;
 import com.github.retrooper.packetevents.event.ProtocolPacketEvent;
 import de.jpx3.intave.check.PlayerCheckPart;
 import de.jpx3.intave.check.world.PlacementAnalysis;
@@ -9,6 +8,8 @@ import de.jpx3.intave.module.linker.bukkit.BukkitEventSubscription;
 import de.jpx3.intave.module.linker.packet.ListenerPriority;
 import de.jpx3.intave.module.linker.packet.PacketSubscription;
 import de.jpx3.intave.module.violation.Violation;
+import de.jpx3.intave.packet.reader.BlockInteractionReader;
+import de.jpx3.intave.packet.reader.PacketReaders;
 import de.jpx3.intave.user.User;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -23,16 +24,12 @@ import static de.jpx3.intave.check.world.PlacementAnalysis.COMMON_FLAG_MESSAGE;
 import static de.jpx3.intave.module.linker.packet.PacketId.Client.*;
 import static de.jpx3.intave.module.violation.Violation.ViolationFlags.DISPLAY_IN_ALL_VERBOSE_MODES;
 
-// this does not work yet, like at all
-
 public final class JumpAndPlace extends PlayerCheckPart<PlacementAnalysis> {
 	private final List<Vector> lastBlocksPlaced = new CopyOnWriteArrayList<>();
 	private boolean startSneakInThisTick;
 	private boolean stopSneakInThisTick;
 	private boolean sneakChangedInThisTick;
 	private boolean placedInThisTick;
-	private boolean isSneaking;
-	private boolean suspicious;
 	private long lastSneakStart;
 	private long lastPlace;
 	private final long lastSneakDuration = 10;
@@ -44,46 +41,24 @@ public final class JumpAndPlace extends PlayerCheckPart<PlacementAnalysis> {
 		super(user, parentCheck);
 	}
 
-	@PacketSubscription(
-		priority = ListenerPriority.HIGH,
-		packetsIn = {
-			FLYING, LOOK, POSITION, POSITION_LOOK
-		}
-	)
+	@PacketSubscription(priority = ListenerPriority.HIGH, packetsIn = {FLYING, LOOK, POSITION, POSITION_LOOK})
 	public void clientTickUpdate(ProtocolPacketEvent event) {
-		Player player = event.getPlayer();
 		if (placedInThisTick || sneakChangedInThisTick) {
-//      player.sendMessage(sneakInThisTick + "("+startSneakInThisTick+","+stopSneakInThisTick+")/" + placedInThisTick);
 			if (placedInThisTick) {
-				// difference to last sneak start
 				long diff = startSneakInThisTick ? 0 : tickCount - lastSneakStart;
-
 				boolean suspiciousSneaking = diff <= 2 && lastSneakDuration <= 2;
-				if (!suspiciousSneaking && violationLevel > 0) {
-					violationLevel -= 0.05;
-				} else if (suspiciousSneaking) {
-					violationLevel += 1;
-				}
-//        player.sendMessage((suspiciousSneaking ? ChatColor.YELLOW : ChatColor.GREEN) + "Sneak start -> Place: " + diff + " last duration: " + lastSneakDuration);
+				if (!suspiciousSneaking && violationLevel > 0) violationLevel -= 0.05;
+				else if (suspiciousSneaking) violationLevel += 1;
 			}
 			if (sneakChangedInThisTick) {
-				// difference to last place
 				long diff = placedInThisTick ? 0 : tickCount - lastPlace;
 				boolean suspiciousSneaking = diff <= 2 && lastSneakDuration <= 2;
-				if (!suspiciousSneaking && violationLevel > 0) {
-					violationLevel -= 0.05;
-				} else if (suspiciousSneaking) {
-					violationLevel += 1;
-				}
-//        player.sendMessage((suspiciousSneaking ? ChatColor.YELLOW : ChatColor.GREEN) +"Place -> Sneak start: " + diff + " last duration: " + lastSneakDuration);
+				if (!suspiciousSneaking && violationLevel > 0) violationLevel -= 0.05;
+				else if (suspiciousSneaking) violationLevel += 1;
 			}
 		}
-		if (startSneakInThisTick) {
-			lastSneakStart = tickCount;
-		}
-		if (placedInThisTick) {
-			lastPlace = tickCount;
-		}
+		if (startSneakInThisTick) lastSneakStart = tickCount;
+		if (placedInThisTick) lastPlace = tickCount;
 		startSneakInThisTick = false;
 		stopSneakInThisTick = false;
 		sneakChangedInThisTick = false;
@@ -92,68 +67,25 @@ public final class JumpAndPlace extends PlayerCheckPart<PlacementAnalysis> {
 		sneakDuration++;
 	}
 
-	@PacketSubscription(
-		priority = ListenerPriority.HIGH,
-		packetsIn = {
-			BLOCK_PLACE
-		}
-	)
+	@PacketSubscription(priority = ListenerPriority.HIGH, packetsIn = BLOCK_PLACE)
 	public void receivePlacementPacket(ProtocolPacketEvent event) {
-		PacketContainer packet = event.getPacket();
 		Player player = event.getPlayer();
-
-		Integer facing = packet.getIntegers().readSafely(0);
-		if (facing == null) {
-			facing = 0;
-		}
-		if (facing == 255) {
-			return;
+		BlockInteractionReader reader = PacketReaders.readerOf(event);
+		try {
+			if (reader.enumDirection() == 255) return;
+		} finally {
+			reader.release();
 		}
 		User user = userOf(player);
 		Material material = user.meta().inventory().heldItemType();
-		boolean hasPlaceable = material.isBlock() && material.isSolid();
-		if (!hasPlaceable) {
-			return;
-		}
+		if (!material.isBlock() || !material.isSolid()) return;
 		placedInThisTick = true;
 	}
-//
-//  @PacketSubscription(
-//    priority = ListenerPriority.HIGH,
-//    packetsIn = {
-//      ENTITY_ACTION_IN
-//    }
-//  )
-//  public void receiveEntityActionPacket(ProtocolPacketEvent event) {
-//    Player player = event.getPlayer();
-//    PacketContainer packet = event.getPacket();
-//    PlayerActionReader reader = PacketReaders.readerOf(packet);
-//
-//    PlayerAction action = reader.playerAction();
-//    switch (action) {
-//      case START_SNEAKING:
-//        startSneakInThisTick = true;
-//        sneakChangedInThisTick = true;
-//        isSneaking = true;
-//        sneakDuration = 0;
-//        break;
-//      case STOP_SNEAKING:
-//        stopSneakInThisTick = true;
-//        sneakChangedInThisTick = true;
-//        isSneaking = false;
-//        lastSneakDuration = sneakDuration;
-//        sneakDuration = 0;
-//        break;
-//    }
-//
-//    reader.release();
-//  }
 
 	@BukkitEventSubscription
 	public void on(BlockPlaceEvent place) {
 		Player player = place.getPlayer();
 		User user = userOf(player);
-
 		if (place.getBlock().getY() < player.getLocation().getBlockY() && isOneLine(lastBlocksPlaced) && blockAgainstWasPlaced(user, place.getBlockAgainst())) {
 			if (violationLevel > 5) {
 				Violation violation = Violation.builderFor(PlacementAnalysis.class)
@@ -167,40 +99,24 @@ public final class JumpAndPlace extends PlayerCheckPart<PlacementAnalysis> {
 		} else {
 			violationLevel = 0;
 		}
-		if (place.isCancelled()) {
-			return;
-		}
-		if (lastBlocksPlaced.size() > 5) {
-			lastBlocksPlaced.remove(0);
-		}
+		if (place.isCancelled()) return;
+		if (lastBlocksPlaced.size() > 5) lastBlocksPlaced.remove(0);
 		lastBlocksPlaced.add(place.getBlock().getLocation().toVector());
 	}
 
 	private boolean isOneLine(List<? extends Vector> blocks) {
-		int lastBlockX = 0,
-			lastBlockY = 0,
-			lastBlockZ = 0;
-		boolean lockedOnX = false,
-			lockedOnZ = false;
-		boolean first = true;
+		int lastBlockX = 0, lastBlockY = 0, lastBlockZ = 0;
+		boolean lockedOnX = false, lockedOnZ = false, first = true;
 		int yTolerance = 2;
 		for (Vector block : blocks) {
 			if (!first) {
 				if (lastBlockY != block.getY()) {
-					if (yTolerance-- <= 0) {
-						return false;
-					}
+					if (yTolerance-- <= 0) return false;
 				} else {
-					if (lastBlockX == block.getX()) {
-						lockedOnX = true;
-					} else if (lockedOnX) {
-						return false;
-					}
-					if (lastBlockZ == block.getZ()) {
-						lockedOnZ = true;
-					} else if (lockedOnZ) {
-						return false;
-					}
+					if (lastBlockX == block.getX()) lockedOnX = true;
+					else if (lockedOnX) return false;
+					if (lastBlockZ == block.getZ()) lockedOnZ = true;
+					else if (lockedOnZ) return false;
 				}
 			}
 			lastBlockX = block.getBlockX();
@@ -214,9 +130,7 @@ public final class JumpAndPlace extends PlayerCheckPart<PlacementAnalysis> {
 	private boolean blockAgainstWasPlaced(User user, Block blockAgainst) {
 		Vector vector = blockAgainst.getLocation().toVector();
 		for (Vector location : lastBlocksPlaced) {
-			if (location.distance(vector) == 0) {
-				return true;
-			}
+			if (location.distance(vector) == 0) return true;
 		}
 		return false;
 	}
