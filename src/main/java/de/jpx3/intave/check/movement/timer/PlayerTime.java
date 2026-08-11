8b8726b9d01a543ea14c3f9ec4a973ca2ecfb8d3
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-11 13:23:27 +0700

    Fix native timer login replay and Bukkit cancellables

--- src/main/java/de/jpx3/intave/check/movement/timer/PlayerTime.java
+++ src/main/java/de/jpx3/intave/check/movement/timer/PlayerTime.java
@@ -36,7 +36,7 @@ import de.jpx3.intave.user.meta.*;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import com.github.retrooper.packetevents.event.CancellableEvent;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
@@ -67,7 +67,6 @@ public class PlayerTime extends MetaCheckPart<Timer, PlayerTime.PlayerTimeMeta> 
       UserRepository.applyOnAll(user -> {
         ConnectionMetadata connectionData = user.meta().connection();
         PlayerTimeMeta checkMeta = metaOf(user);
-        //noinspection deprecation
         Modules.feedback().synchronize(user.player(), System.nanoTime(), (unused, time) -> {
           connectionData.queueToNextTransaction(() -> {
             checkMeta.time = Math.max(checkMeta.time, checkMeta.limitToBeApplied);
@@ -78,21 +77,14 @@ public class PlayerTime extends MetaCheckPart<Timer, PlayerTime.PlayerTimeMeta> 
     }, 0, 1);
   }
 
-  @PacketSubscription(
-    priority = ListenerPriority.HIGHEST,
-    packetsOut = {
-      LOGIN
-    }
-  )
+  @PacketSubscription(priority = ListenerPriority.HIGHEST, packetsOut = LOGIN)
   public void receiveLogin(ProtocolPacketEvent event) {
-    Player player = event.getPlayer();
-    if (player == null) {
-      return;
-    }
+    Player player = (Player) event.getPlayer();
+    if (player == null) return;
     User user = userOf(player);
     PlayerTimeMeta checkMeta = metaOf(user);
     playerJoinTimeCache.put(player.getUniqueId(), System.nanoTime());
-    PacketSender.sendServerPacketWithoutEvent(player, event.getPacket());
+    PacketSender.sendServerPacketWithoutEvent(player, event.getFullBufferClone());
     user.tickFeedback(() -> checkMeta.gameJoinReceived = true);
     event.setCancelled(true);
   }
@@ -108,139 +100,79 @@ public class PlayerTime extends MetaCheckPart<Timer, PlayerTime.PlayerTimeMeta> 
     }
   }
 
-  @BukkitEventSubscription
-  public void on(PlayerQuitEvent quit) {
-    playerJoinTimeCache.remove(quit.getPlayer().getUniqueId());
-  }
+  @BukkitEventSubscription public void on(PlayerQuitEvent quit) { playerJoinTimeCache.remove(quit.getPlayer().getUniqueId()); }
 
   @DispatchTarget
   public void receiveMovement(ProtocolPacketEvent event) {
-    Player player = event.getPlayer();
-    if (player == null) {
-      return;
-    }
+    Player player = (Player) event.getPlayer();
+    if (player == null) return;
     User user = userOf(player);
     PlayerTimeMeta checkMeta = metaOf(user);
     MetadataBundle bundle = user.meta();
     MovementMetadata movementData = bundle.movement();
     AbilityMetadata abilityData = user.meta().abilities();
-
-    // Time was not loaded yet
-    if (checkMeta.time == -1) {
-      // The default fallback time should never be used but let's provide it if something goes horribly wrong
-      checkMeta.time = playerJoinTimeCache.getOrDefault(player.getUniqueId(), System.nanoTime());
-    }
-    // Exclude players in certain states such as creative, spectator or teleport
-    // We also have to check if the player received the initial join packet due to proxies doing weird things
+    if (checkMeta.time == -1) checkMeta.time = playerJoinTimeCache.getOrDefault(player.getUniqueId(), System.nanoTime());
     if (!checkMeta.gameJoinReceived || movementData.ticksPast(TELEPORT) == 0
-      || abilityData.inGameModeIncludePending(AbilityTracker.GameMode.CREATIVE) || abilityData.ignoringMovementPackets() || user.meta().movement().isInVehicle()) {
-      return;
-    }
+      || abilityData.inGameModeIncludePending(AbilityTracker.GameMode.CREATIVE)
+      || abilityData.ignoringMovementPackets() || movementData.isInVehicle()) return;
 
-    checkMeta.time += 50_000_000; // allow constant 0.05ms clock error = give 1s every 2000s (33min)
+    checkMeta.time += 50_000_000;
     checkMeta.limitToBeApplied = checkMeta.queuedLimit;
     long diff = checkMeta.time - System.nanoTime();
     statisticApply(user, CheckStatistics::increaseTotal);
-    double experimentalLimit = (-user.latency()) * 1_000_000f;
-
-    boolean bad = diff > experimentalLimit + 25_000_000;
-
-//    player.sendMessage((bad ? ChatColor.RED : ChatColor.GRAY) + " " + (diff / (50 * 1_000_000f)) + " / " + (experimentalLimit / (50 * 1_000_000f)));
-
     int limit = 40_000_000;
-    if ((diff > limit) && !user.meta().movement().isInVehicle()) {
-      double displayValue = diff / (50 * 1_000_000f);
-      if (displayValue < 0.01) {
-        displayValue = 0.01;
-      }
-      String balanceAsString = formatDouble(displayValue, 2);
+    if (diff > limit && !movementData.isInVehicle()) {
+      double displayValue = Math.max(0.01, diff / (50 * 1_000_000f));
       Violation violation = Violation.builderFor(Timer.class).forPlayer(player)
         .withMessage("moved too frequently")
-        .withDetails(balanceAsString + " ticks ahead")
-        .withVL(Math.min(Math.max(displayValue * 3f, 1), 5))
-        .build();
+        .withDetails(formatDouble(displayValue, 2) + " ticks ahead")
+        .withVL(Math.min(Math.max(displayValue * 3f, 1), 5)).build();
       ViolationContext violationContext = Modules.violationProcessor().processViolation(violation);
       if (violationContext.shouldCounterThreat()) {
         checkMeta.lastTimerFlag = System.currentTimeMillis();
         movementData.invalidMovement = true;
         statisticApply(user, CheckStatistics::increaseFails);
         Modules.mitigate().movement().emulationSetBack(player, Motion.newEmpty(), 3, 2, false);
-        if (violationContext.violationLevelAfter() > 50) {
-          user.nerfPermanently(AttackNerfStrategy.DMG_HIGH, "timer");
-        }
+        if (violationContext.violationLevelAfter() > 50) user.nerfPermanently(AttackNerfStrategy.DMG_HIGH, "timer");
       }
       checkMeta.time -= 10_000_000;
       return;
     } else if (System.currentTimeMillis() - checkMeta.lastTimerFlag > 10000) {
       decrementer.decrement(user, 0.005);
     }
+
     int blinkLimit = parentCheck().blinkLimit();
-    if (!checkMeta.inTeleport && !user.justJoined() && !user.meta().abilities().probablyFlying() && blinkLimit > 0 && diff < -Math.abs(blinkLimit * 50_000_000L)/*-2_500_000_000L*/) {
+    if (!checkMeta.inTeleport && !user.justJoined() && !user.meta().abilities().probablyFlying()
+      && blinkLimit > 0 && diff < -Math.abs(blinkLimit * 50_000_000L)) {
       checkMeta.inTeleport = true;
-      user.tickFeedback(() -> {
-        checkMeta.inTeleport = false;
-      }, FeedbackOptions.SELF_SYNCHRONIZATION);
+      user.tickFeedback(() -> checkMeta.inTeleport = false, FeedbackOptions.SELF_SYNCHRONIZATION);
       double displayValue = -diff / (50 * 1_000_000f);
-      String balanceAsString = formatDouble(displayValue, 2);
       Violation violation = Violation.builderFor(Timer.class).forPlayer(player)
         .withMessage("is halting game ticks")
-        .withDetails(balanceAsString + " ticks behind")
-        .withVL(0)
-        .build();
-	    Modules.violationProcessor().processViolation(violation);
-	    Motion setback = Motion.newEmpty();
-      Modules.mitigate().movement().emulationSetBack(player, setback, 1, 2, false);
+        .withDetails(formatDouble(displayValue, 2) + " ticks behind").withVL(0).build();
+      Modules.violationProcessor().processViolation(violation);
+      Modules.mitigate().movement().emulationSetBack(player, Motion.newEmpty(), 1, 2, false);
     }
     statisticApply(user, CheckStatistics::increasePasses);
   }
 
-  @BukkitEventSubscription
-  public void receiveItemConsume(PlayerItemConsumeEvent event) {
-    Player player = event.getPlayer();
-    cancelOnPacketOverflow(player, event);
-  }
-
-  @BukkitEventSubscription
-  public void receiveBowShoot(EntityShootBowEvent event) {
-    Entity entity = event.getEntity();
-    if (entity instanceof Player) {
-      cancelOnPacketOverflow((Player) entity, event);
-    }
-  }
-
-  @BukkitEventSubscription
-  public void receiveHealthUpdate(EntityRegainHealthEvent event) {
-    Entity entity = event.getEntity();
-    if (entity instanceof Player) {
-      cancelOnPacketOverflow((Player) entity, event);
-    }
-  }
-
-  @BukkitEventSubscription
-  public void receiveAttackUpdate(EntityDamageByEntityEvent event) {
+  @BukkitEventSubscription public void receiveItemConsume(PlayerItemConsumeEvent event) { cancelOnPacketOverflow(event.getPlayer(), event); }
+  @BukkitEventSubscription public void receiveBowShoot(EntityShootBowEvent event) { if (event.getEntity() instanceof Player) cancelOnPacketOverflow((Player) event.getEntity(), event); }
+  @BukkitEventSubscription public void receiveHealthUpdate(EntityRegainHealthEvent event) { if (event.getEntity() instanceof Player) cancelOnPacketOverflow((Player) event.getEntity(), event); }
+  @BukkitEventSubscription public void receiveAttackUpdate(EntityDamageByEntityEvent event) {
     Entity entity = event.getDamager();
     if (entity instanceof Player && event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
       Player player = (Player) entity;
-      int attackCancelThreshold = trustFactorSetting("act", player);
-      int attackCancelLength = trustFactorSetting("acl", player);
-      cancelOnPacketOverflow(player, event, attackCancelThreshold, attackCancelLength);
-
-      if ((System.currentTimeMillis() - metaOf(userOf(player)).lastTimerFlag) < 600) {
-        event.setCancelled(true);
-      }
+      cancelOnPacketOverflow(player, event, trustFactorSetting("act", player), trustFactorSetting("acl", player));
+      if (System.currentTimeMillis() - metaOf(userOf(player)).lastTimerFlag < 600) event.setCancelled(true);
     }
   }
 
-  private void cancelOnPacketOverflow(Player player, CancellableEvent cancellable) {
-    cancelOnPacketOverflow(player, cancellable, DEFAULT_THRESHOLD, DEFAULT_DELAY);
-  }
-
-  private void cancelOnPacketOverflow(Player player, CancellableEvent cancellable, long threshold, long delay) {
+  private void cancelOnPacketOverflow(Player player, Cancellable cancellable) { cancelOnPacketOverflow(player, cancellable, DEFAULT_THRESHOLD, DEFAULT_DELAY); }
+  private void cancelOnPacketOverflow(Player player, Cancellable cancellable, long threshold, long delay) {
     User user = userOf(player);
     PlayerTimeMeta timerData = metaOf(user);
-    long lastTimerFlag = timerData.lastTimerFlag;
-    long msSinceFlag = System.currentTimeMillis() - lastTimerFlag;
-    if (violationLevelOf(user) > threshold && msSinceFlag < delay) {
+    if (violationLevelOf(user) > threshold && System.currentTimeMillis() - timerData.lastTimerFlag < delay) {
       cancellable.setCancelled(true);
       player.updateInventory();
     }
@@ -250,11 +182,8 @@ public class PlayerTime extends MetaCheckPart<Timer, PlayerTime.PlayerTimeMeta> 
     ViolationMetadata violationLevelData = user.meta().violationLevel();
     Map<String, Map<String, Double>> violationLevel = violationLevelData.violationLevel;
     String name = name().toLowerCase();
-    if (!violationLevel.containsKey(name)) {
-      return 0;
-    }
-    Map<String, Double> stringDoubleMap = violationLevel.get(name);
-    return stringDoubleMap.get("thresholds");
+    if (!violationLevel.containsKey(name)) return 0;
+    return violationLevel.get(name).get("thresholds");
   }
 
   public static class PlayerTimeMeta extends CheckCustomMetadata {
@@ -263,6 +192,6 @@ public class PlayerTime extends MetaCheckPart<Timer, PlayerTime.PlayerTimeMeta> 
     public long limitToBeApplied = System.nanoTime();
     public boolean gameJoinReceived;
     public long lastTimerFlag;
-    public boolean inTeleport = false;
+    public boolean inTeleport;
   }
 }
