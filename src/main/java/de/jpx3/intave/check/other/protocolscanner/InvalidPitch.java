55b64ecda58969ef7bd25168e413bda1ceb30f4e
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-11 17:19:16 +0700

    Migrate invalid pitch guard to native movement reader

--- src/main/java/de/jpx3/intave/check/other/protocolscanner/InvalidPitch.java
+++ src/main/java/de/jpx3/intave/check/other/protocolscanner/InvalidPitch.java
@@ -7,6 +7,8 @@ import de.jpx3.intave.math.MathHelper;
 import de.jpx3.intave.module.Modules;
 import de.jpx3.intave.module.linker.packet.PacketSubscription;
 import de.jpx3.intave.module.violation.Violation;
+import de.jpx3.intave.packet.reader.PacketReaders;
+import de.jpx3.intave.packet.reader.PlayerMoveReader;
 import org.bukkit.entity.Player;
 
 import static de.jpx3.intave.module.linker.packet.PacketId.Client.LOOK;
@@ -24,16 +26,21 @@ public final class InvalidPitch extends CheckPart<ProtocolScanner> {
   )
   public void receiveRotation(ProtocolPacketEvent event) {
     Player player = event.getPlayer();
-    float rotationPitch = event.getPacket().getFloat().read(1);
-    if (Math.abs(rotationPitch) > 90.000001f) {
-      event.getPacket().getFloat().writeSafely(1, 0f);
-      String message = "sent invalid rotation";
-      String details = "pitch at " + MathHelper.formatDouble(rotationPitch, 4);
-      Violation violation = Violation.builderFor(ProtocolScanner.class)
-        .forPlayer(player).withMessage(message).withDetails(details)
-        .withVL(100)
-        .build();
-      Modules.violationProcessor().processViolation(violation);
+    PlayerMoveReader reader = PacketReaders.readerOf(event);
+    try {
+      float rotationPitch = reader.pitch();
+      if (Math.abs(rotationPitch) > 90.000001f) {
+        reader.setPitch(0f);
+        String message = "sent invalid rotation";
+        String details = "pitch at " + MathHelper.formatDouble(rotationPitch, 4);
+        Violation violation = Violation.builderFor(ProtocolScanner.class)
+          .forPlayer(player).withMessage(message).withDetails(details)
+          .withVL(100)
+          .build();
+        Modules.violationProcessor().processViolation(violation);
+      }
+    } finally {
+      reader.release();
     }
   }
 }
