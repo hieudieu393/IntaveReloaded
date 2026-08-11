fdcb25957f8ee3576e8b07bb1dc8c6dcf4e059a6
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-11 12:45:54 +0700

    Use native PacketEvents plugin messages

--- src/main/java/de/jpx3/intave/connect/customclient/CustomClientSupportService.java
+++ src/main/java/de/jpx3/intave/connect/customclient/CustomClientSupportService.java
@@ -1,19 +1,13 @@
 package de.jpx3.intave.connect.customclient;
 
-import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.protocol.packettype.PacketTypeCommon;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.PacketContainer;
-import de.jpx3.intave.share.MinecraftKey;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPluginMessage;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import de.jpx3.intave.IntaveLogger;
 import de.jpx3.intave.IntavePlugin;
-import de.jpx3.intave.adapter.MinecraftVersions;
 import de.jpx3.intave.check.EventProcessor;
 import de.jpx3.intave.connect.sibyl.LabyModChannelHelper;
 import de.jpx3.intave.executor.Synchronizer;
-import de.jpx3.intave.klass.Lookup;
 import de.jpx3.intave.module.Modules;
 import de.jpx3.intave.module.linker.packet.PacketSubscription;
 import de.jpx3.intave.packet.PacketSender;
@@ -22,7 +16,6 @@ import de.jpx3.intave.user.User;
 import de.jpx3.intave.user.UserRepository;
 import de.jpx3.intave.user.meta.ConnectionMetadata;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -83,20 +76,8 @@ public final class CustomClientSupportService implements EventProcessor {
   }
 
   private void sendCustomDataPacket(Player player, String channel, String data, String prefix, String key) {
-    PacketContainer packetContainer = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.PLUGIN_MESSAGE);
-    if (MinecraftVersions.VER1_13_0.atOrAbove()) {
-      packetContainer.getMinecraftKeys().write(0, new MinecraftKey(prefix, key));
-    } else {
-      packetContainer.getStrings().write(0, prefix + ":" + key);
-    }
-    try {
-      //noinspection unchecked
-      Class<Object> packetDataSerializerClass = (Class<Object>) Lookup.serverClass("PacketDataSerializer");
-      Object packetDataSerializer = packetDataSerializerClass.getConstructor(ByteBuf.class).newInstance(Unpooled.wrappedBuffer(LabyModChannelHelper.getBytesToSend(channel, data)));
-      packetContainer.getSpecificModifier(packetDataSerializerClass).write(0, packetDataSerializer);
-      Synchronizer.synchronize(() -> PacketSender.sendServerPacket(player, packetContainer));
-    } catch (Exception exception) {
-      exception.printStackTrace();
-    }
+    byte[] payload = LabyModChannelHelper.getBytesToSend(channel, data);
+    WrapperPlayServerPluginMessage packet = new WrapperPlayServerPluginMessage(prefix + ":" + key, payload);
+    Synchronizer.synchronize(() -> PacketSender.sendServerPacket(player, packet));
   }
 }
