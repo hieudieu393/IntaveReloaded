3cf958d8acf33d0e752aadcd2178778a05124d7b
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-11 12:55:05 +0700

    Migrate civbreak heuristic to BlockDigReader

--- src/main/java/de/jpx3/intave/check/combat/heuristics/other/CivbreakHeuristic.java
+++ src/main/java/de/jpx3/intave/check/combat/heuristics/other/CivbreakHeuristic.java
@@ -1,56 +1,35 @@
 package de.jpx3.intave.check.combat.heuristics.other;
 
-import com.github.retrooper.packetevents.protocol.player.DiggingAction;
-import com.comphenix.protocol.events.PacketContainer;
 import com.github.retrooper.packetevents.event.ProtocolPacketEvent;
-import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.github.retrooper.packetevents.protocol.player.DiggingAction;
 import de.jpx3.intave.check.MetaCheckPart;
 import de.jpx3.intave.check.combat.Heuristics;
 import de.jpx3.intave.module.linker.packet.PacketSubscription;
+import de.jpx3.intave.packet.reader.BlockDigReader;
+import de.jpx3.intave.packet.reader.PacketReaders;
 import de.jpx3.intave.user.User;
 import de.jpx3.intave.user.meta.CheckCustomMetadata;
 import de.jpx3.intave.user.meta.ProtocolMetadata;
-import org.bukkit.entity.Player;
 
 import static de.jpx3.intave.module.linker.packet.PacketId.Client.BLOCK_DIG;
 
 public final class CivbreakHeuristic extends MetaCheckPart<Heuristics, CivbreakHeuristic.CivbreakMeta> {
-
   public CivbreakHeuristic(Heuristics parentCheck) {
     super(parentCheck, CivbreakMeta.class);
   }
 
-  /*
-  What is civbreak?
-  Civbreak abuses a server bug where you can instant break a block on a block position where you
-  already destroyed one block. So civbreak only sends multiple STOP_DESTROY_BLOCK packets after the
-  player destroyed the block once.
-   */
-  @PacketSubscription(
-    packetsIn = {
-      BLOCK_DIG
-    }
-  )
+  @PacketSubscription(packetsIn = BLOCK_DIG)
   public void receiveInteractionPacket(ProtocolPacketEvent event) {
-    Player player = event.getPlayer();
-    User user = userOf(player);
+    User user = userOf(event.getPlayer());
     CivbreakMeta meta = metaOf(user);
-    PacketContainer packet = event.getPacket();
-    DiggingAction playerDigType = packet.getPlayerDigTypes().readSafely(0);
-    // Note: isMining should set to false on every PlayerDigType except START_DESTROY_BLOCK
-//    player.sendMessage("" + playerDigType);
-    if (playerDigType == DiggingAction.START_DESTROY_BLOCK) {
+    BlockDigReader reader = PacketReaders.readerOf(event);
+    DiggingAction action = reader.action();
+    if (action == DiggingAction.START_DIGGING) {
       meta.isMining = true;
     }
-    if (playerDigType == DiggingAction.STOP_DESTROY_BLOCK) {
-      if (user.protocolVersion() < ProtocolMetadata.VER_1_14) {
-        if (!meta.isMining) {
-//          player.sendMessage("cancel");
-          event.setCancelled(true);
-        }
-      } else {
-        // TODO: fix civbreak on 1.14+
-        // players don't send a start break packet when destroying a block multiple times on 1.14+
+    if (action == DiggingAction.FINISHED_DIGGING) {
+      if (user.protocolVersion() < ProtocolMetadata.VER_1_14 && !meta.isMining) {
+        event.setCancelled(true);
       }
       meta.isMining = false;
     }
