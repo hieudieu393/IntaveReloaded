d9414030b266a34611a90291509f03a9e255a6ba
Onxe <57979356+hieudieu393@users.noreply.github.com>
2026-08-11 13:08:37 +0700

    Migrate client settings scanner to PacketEvents wrapper

--- src/main/java/de/jpx3/intave/check/other/protocolscanner/SkinBlinker.java
+++ src/main/java/de/jpx3/intave/check/other/protocolscanner/SkinBlinker.java
@@ -2,21 +2,16 @@
  * Copyright 2026 Intave
  *
  * This software is licensed under the PolyForm Perimeter License 1.0.0.
- * You may use this software for any purpose, except for providing to
- * others any product that competes with the software.
- *
- * A copy of the license is available at:
- *   https://polyformproject.org/licenses/perimeter/1.0.0/
  */
-
 package de.jpx3.intave.check.other.protocolscanner;
 
-import com.comphenix.protocol.events.PacketContainer;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.event.ProtocolPacketEvent;
+import com.github.retrooper.packetevents.protocol.player.HumanoidArm;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientSettings;
 import de.jpx3.intave.adapter.MinecraftVersions;
 import de.jpx3.intave.check.CheckPart;
 import de.jpx3.intave.check.other.ProtocolScanner;
-import de.jpx3.intave.klass.Lookup;
 import de.jpx3.intave.math.Hypot;
 import de.jpx3.intave.module.linker.packet.PacketSubscription;
 import de.jpx3.intave.user.User;
@@ -29,55 +24,35 @@ import static de.jpx3.intave.module.linker.packet.PacketId.Client.SETTINGS;
 
 public final class SkinBlinker extends CheckPart<ProtocolScanner> {
   private static final boolean HAS_OFF_HAND = MinecraftVersions.VER1_9_0.atOrAbove();
-  private static Class<?> enumMainHandClass;
 
   public SkinBlinker(ProtocolScanner parentCheck) {
     super(parentCheck);
   }
 
-  @PacketSubscription(
-    packetsIn = {
-      SETTINGS
-    }
-  )
+  @PacketSubscription(packetsIn = SETTINGS)
   public void receiveClientOptions(ProtocolPacketEvent event) {
     Player player = event.getPlayer();
     User user = userOf(player);
+    if (MinecraftVersions.VER1_20_2.atOrAbove()) return;
 
-    if (MinecraftVersions.VER1_20_2.atOrAbove()) {
-      return;
-    }
-
-    PacketContainer packet = event.getPacket();
     ProtocolMetadata clientData = user.meta().protocol();
     if (HAS_OFF_HAND && clientData.combatUpdate()) {
-      if (enumMainHandClass == null) {
-        enumMainHandClass = Lookup.serverClass("EnumMainHand");
-      }
-      HandSlot sentHand = packet.getEnumModifier(HandSlot.class, enumMainHandClass).read(0);
-      if (!equalHand(player.getMainHand(), sentHand)) {
-        return;
-      }
+      WrapperPlayClientSettings settings = new WrapperPlayClientSettings((PacketReceiveEvent) event);
+      if (!equalHand(player.getMainHand(), settings.getMainHand())) return;
     }
+
     MovementMetadata movementData = user.meta().movement();
     int keyForward = movementData.keyForward;
     int keyStrafe = movementData.keyStrafe;
     double distanceMoved = Hypot.fast(movementData.offsetMotionX(), movementData.offsetMotionZ());
-    if (movementData.inWeb || movementData.receivedFlyingPacketIn(2)) {
-      return;
-    }
+    if (movementData.inWeb || movementData.receivedFlyingPacketIn(2)) return;
     if ((keyForward != 0 || keyStrafe != 0) && distanceMoved > 0.1) {
       event.setCancelled(true);
     }
   }
 
-  private boolean equalHand(Object bukkitHand, HandSlot hand) {
-    return bukkitHand == MainHand.LEFT && hand == HandSlot.LEFT
-      || bukkitHand == MainHand.RIGHT && hand == HandSlot.RIGHT;
-  }
-
-  public enum HandSlot {
-    LEFT,
-    RIGHT
+  private boolean equalHand(MainHand bukkitHand, HumanoidArm hand) {
+    return bukkitHand == MainHand.LEFT && hand == HumanoidArm.LEFT
+      || bukkitHand == MainHand.RIGHT && hand == HumanoidArm.RIGHT;
   }
 }
